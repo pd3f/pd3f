f1bb4da60ababa73970706e752e75ed8ec0d8644
Johannes Filter <hi@jfilter.de>
2020-06-23 16:57:09 +0200

    remove leftover

--- test2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl --header "Content-Type: application/json" \
-    --request POST \
-    --data '{"texts":["Ich lebe in Berlin", "Ich lebe in Auto"]}' \
-    http://localhost:5000/score_async
